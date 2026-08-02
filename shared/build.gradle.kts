c146e902d005e0a00318986bda4d74608f0640ed
Abdallah Elbeheiry <abdallahelbhary39@gmail.com>
2026-08-02 20:22:57 +0300

    Phase 1: Add material3-window-size-class dependency

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -65,6 +65,7 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.compose.material3.window.size.class)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
